8ad0d4c7f4f913e5b74e82d1bf1552b4d20b2668
chaeeun1103 <chaeeun1103@inu.ac.kr>
2024-11-04 16:57:48 +0900

    BookController 구현

---
+++ practice/chaeeun/demo/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.Book;
+import com.example.demo.service.BookService;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book){
+        return bookService.saveBook(book);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Book> getBookId(@PathVariable Long id){
+        return bookService.findById(id);
+    }
+
+    @GetMapping("/title")
+    public Optional<Book> getBookTitle(@RequestParam String title){
+        return bookService.findByTitle(title);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable Long id){
+        bookService.deleteById(id);
+        return id + "번 도서가 삭제되었습니다.";
+    }
+
+}
