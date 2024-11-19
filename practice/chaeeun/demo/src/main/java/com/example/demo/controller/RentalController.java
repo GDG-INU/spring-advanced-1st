ef1baff367096ea61aead13f0f90fea3528bd0e8
chaeeun1103 <chaeeun1103@inu.ac.kr>
2024-11-20 02:16:33 +0900

    Rental 구현, 트랜잭션을 이용한 RentalService 구현

--- practice/chaeeun/demo/src/main/java/com/example/demo/controller/RentalController.java
+++ practice/chaeeun/demo/src/main/java/com/example/demo/controller/RentalController.java
@@ -1,4 +1,38 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.RentalDTO;
+import com.example.demo.service.RentalService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rentals")
 public class RentalController {
+    private final RentalService rentalService;
+
+    public RentalController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
+
+    @PostMapping
+    public ResponseEntity<RentalDTO> rentBook(@RequestBody RentalDTO rentalDTO) {
+        RentalDTO createdRentalDTO = rentalService.registerRental(rentalDTO.getMemberId(), rentalDTO.getBookId());
+        return ResponseEntity.ok(createdRentalDTO);
+    }
+
+    @PutMapping("/{rentalId}/return")
+    public ResponseEntity<RentalDTO> returnBook(@PathVariable Long rentalId) {
+        RentalDTO rentalDTO = rentalService.returnBook(rentalId);
+        return ResponseEntity.ok(rentalDTO);
+    }
+
+    // 특정 회원이 rental중인 책 리스트 목록
+    @GetMapping("/member/{memberId}")
+    public ResponseEntity<List<RentalDTO>> getAllRentals(@PathVariable Long memberId) {
+        List<RentalDTO> rentals = rentalService.getAllRentals(memberId);
+        return ResponseEntity.ok(rentals);
+    }
+
 }
--- practice/chaeeun/demo/src/main/java/com/example/demo/repository/RentalRepository.java
+++ practice/chaeeun/demo/src/main/java/com/example/demo/repository/RentalRepository.java
@@ -1,4 +1,14 @@
 package com.example.demo.repository;
 
-public interface RentalRepository {
+import com.example.demo.domain.Book;
+import com.example.demo.domain.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+    // 특정 책이 대여중인지 확인
+    // Optional<Rental> findByBookAndReturnDateIsNull(Book book);
+    // 현재 대여 중인 책을 조회 (반납 날짜가 null인 경우)
+    List<Rental> findByMemberIdAndReturnDateIsNull(Long memberId);
 }
