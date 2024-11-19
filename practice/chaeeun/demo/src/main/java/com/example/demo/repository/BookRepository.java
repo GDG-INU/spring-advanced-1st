138bfb51f2a21843d869923dc6c84a59c6f76d01
chaeeun1103 <chaeeun1103@inu.ac.kr>
2024-11-19 22:42:09 +0900

    연관 엔티티 fetch join 적용, JPQL

--- practice/chaeeun/demo/src/main/java/com/example/demo/repository/BookRepository.java
+++ practice/chaeeun/demo/src/main/java/com/example/demo/repository/BookRepository.java
@@ -2,10 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     // 특정 단어가 포함된 책 검색
+    @Query("SELECT b FROM Book b JOIN FETCH b.author JOIN FETCH b.publisher WHERE b.title LIKE %:title%")
     List<Book> findByTitleContaining(String title);
 }
