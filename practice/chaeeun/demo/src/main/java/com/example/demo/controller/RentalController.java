5f142664731ff313ceaa8135579f0e911a587aae
chaeeun1103 <chaeeun1103@inu.ac.kr>
2024-11-23 14:46:20 +0900

    Rental @Slf4j 애노테이션을 사용한 log 설정

--- practice/chaeeun/demo/src/main/java/com/example/demo/controller/RentalController.java
+++ practice/chaeeun/demo/src/main/java/com/example/demo/controller/RentalController.java
@@ -2,10 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.RentalDTO;
 import com.example.demo.service.RentalService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/rentals")
 public class RentalController {
@@ -17,20 +19,26 @@ public class RentalController {
 
     @PostMapping
     public ResponseEntity<RentalDTO> rentBook(@RequestBody RentalDTO rentalDTO) {
+        log.info("책 대여 요청: memberId={}, bookId={}", rentalDTO.getMemberId(), rentalDTO.getBookId());
         RentalDTO createdRentalDTO = rentalService.registerRental(rentalDTO.getMemberId(), rentalDTO.getBookId());
+        log.info("책 대여 성공 : {}", createdRentalDTO);
         return ResponseEntity.ok(createdRentalDTO);
     }
 
     @PutMapping("/{rentalId}/return")
     public ResponseEntity<RentalDTO> returnBook(@PathVariable Long rentalId) {
+        log.info("책 반납 요청: rentalId = {}", rentalId);
         RentalDTO rentalDTO = rentalService.returnBook(rentalId);
+        log.info("책 반납 성공 : {}", rentalDTO);
         return ResponseEntity.ok(rentalDTO);
     }
 
     // 특정 회원이 rental중인 책 리스트 목록
     @GetMapping("/member/{memberId}")
     public ResponseEntity<List<RentalDTO>> getAllRentals(@PathVariable Long memberId) {
+        log.info("회원이 대여하고 있는 책 목록 요청 : memberId = {}", memberId);
         List<RentalDTO> rentals = rentalService.getAllRentals(memberId);
+        log.info("회원이 대여하고 있는 책 목록 조회 성공: 대여 건수={}, memberId={}", rentals.size(), memberId);
         return ResponseEntity.ok(rentals);
     }
 
--- practice/chaeeun/demo/src/main/java/com/example/demo/service/RentalService.java
+++ practice/chaeeun/demo/src/main/java/com/example/demo/service/RentalService.java
@@ -10,6 +10,7 @@ import com.example.demo.exception.NotFoundException;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.RentalRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class RentalService {
     private final RentalRepository rentalRepository;
@@ -32,21 +34,29 @@ public class RentalService {
     // 책 대여
     @Transactional
     public RentalDTO registerRental(Long memberId, Long bookId){
+        log.debug("책 대여 처리를 시작합니다. memberId={}, bookId={}", memberId, bookId);
         Member member = memberRepository.findById(memberId)
-           .orElseThrow(() -> new NotFoundException("회원이 존재하지 않습니다."));
+           .orElseThrow(() -> {
+               log.error("존재하지 않는 회원으로 대여에 실패했습니다. memberId={}", memberId);
+               return new NotFoundException("회원이 존재하지 않습니다.");
+           });
 
         Book book = bookRepository.findById(bookId)
-            .orElseThrow(() -> new NotFoundException("책이 존재하지 않습니다."));
+            .orElseThrow(() -> {
+                log.error("존재하지 않는 회원으로 대여에 실패했습니다. bookId={}", bookId);
+                return new NotFoundException("책이 존재하지 않습니다.");
+            });
 
         // 책이 이미 대여중인지 확인(returnDate가 null이라면 아직 반납되지 않은 상태)
         if(rentalRepository.findByBookAndReturnDateIsNull(book).isPresent()){
+            log.warn("이미 대여중인 책은 대여가 불가합니다. bookId={}", bookId);
            throw new AlreadyRentedException("이 책은 이미 대여중이므로 대여할 수 없습니다.");
         }
 
         // 정적 팩토리 메서드를 사용해 객체 생성
         Rental rental = Rental.createRental(member, book);
-
         Rental savedRental = rentalRepository.save(rental);
+        log.info("책 대여에 성공했습니다. rentalId={}, memberId={}, bookId={}", rental.getId(), memberId, bookId);
 
        return RentalDTO.from(savedRental);
     }
@@ -54,12 +64,16 @@ public class RentalService {
     // 책 반납
     @Transactional
     public RentalDTO returnBook(Long rentalId){
+        log.debug("책 반납 처리를 시작합니다. rentalId={} ", rentalId);
         Rental rental = rentalRepository.findWithMemberAndBookById(rentalId)
-                .orElseThrow(() -> new NotFoundException("대여 기록이 존재하지 않습니다."));
+                .orElseThrow(() -> {
+                    log.error("존재하지 않는 대여 기록으로 반납에 실패했습니다. rentalId={}", rentalId);
+                    return new NotFoundException("대여 기록이 존재하지 않습니다.");
+                });
 
         rental.markAsReturned();
-
         Rental savedRental = rentalRepository.save(rental);
+        log.info("책 반납에 성공했습니다. rentalId={}, returnData={}", rentalId, savedRental.getReturnDate());
 
         return RentalDTO.from(savedRental);
 
@@ -67,8 +81,12 @@ public class RentalService {
 
     // 현재 대여중인 책 조회
     public List<RentalDTO> getAllRentals(Long memberId) {
+        log.debug("회원 대여 목록 조회를 시작합니다. memberId={}", memberId);
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException(memberId + " 는 존재하지 않습니다."));
+                .orElseThrow(() -> {
+                    log.error("존재하지 않는 회원입니다.memberId={}", memberId);
+                return new IllegalArgumentException(memberId + " 는 존재하지 않습니다.");
+                });
 
         return rentalRepository.findByMemberIdAndReturnDateIsNull(memberId).stream()
                 .map(rental -> new RentalDTO(
@@ -81,5 +99,6 @@ public class RentalService {
                 ))
                 .collect(Collectors.toList());
     }
+
 }
 
